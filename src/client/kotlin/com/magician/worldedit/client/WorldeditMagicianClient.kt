b36301dc47b1a1011b643d9312ef9eb9bedfb77a
FRIEDparrot <814680344@qq.com>
2026-07-25 00:50:29 +0800

    init

---
+++ src/client/kotlin/com/magician/worldedit/client/WorldeditMagicianClient.kt
@@ -0,0 +1,9 @@
+package com.magician.worldedit.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object WorldeditMagicianClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
