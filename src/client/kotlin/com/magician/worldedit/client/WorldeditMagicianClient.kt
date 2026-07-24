2db8ee56b66e29b013f3ad7bf3f92fcce87b6418
FRIEDparrot <814680344@qq.com>
2026-07-25 01:20:12 +0800

    add test command

--- src/client/kotlin/com/magician/worldedit/client/WorldeditMagicianClient.kt
+++ src/client/kotlin/com/magician/worldedit/client/WorldeditMagicianClient.kt
@@ -1,9 +1,21 @@
 package com.magician.worldedit.client
 
+import com.mojang.brigadier.Command
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+import net.minecraft.network.chat.Component
 
 object WorldeditMagicianClient : ClientModInitializer {
 	override fun onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+			dispatcher.register(
+				literal("tests").executes {
+						context -> context.source.sendFeedback(Component.literal("tests"))
+					Command.SINGLE_SUCCESS
+				}
+			)
+		}
 	}
 }
